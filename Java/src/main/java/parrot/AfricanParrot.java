617abe296b03878990bafbbe958d3a6ad6c2aeb5
Gregor Riegler <gregor.riegler@ebcont.com>
2023-02-10 14:25:25 +0100

    wip

--- Java/src/main/java/parrot/AfricanParrot.java
+++ Java/src/main/java/parrot/AfricanParrot.java
@@ -1,12 +1,7 @@
 package parrot;
 
-public class AfricanParrot implements Parrot {
+public class AfricanParrot {
     static ParrotImpl createAfricanParrot(int numberOfCoconuts) {
         return new ParrotImpl(ParrotTypeEnum.AFRICAN, numberOfCoconuts, 0, false);
-    }
-
-    @Override
-    public double getSpeed() {
-        return 0;
     }
 }
