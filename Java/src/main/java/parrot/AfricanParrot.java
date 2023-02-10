c2df6e3688e4c1b613501eaa79745f08f590230a
Gregor Riegler <gregor.riegler@ebcont.com>
2023-02-10 14:33:40 +0100

    wip

--- Java/src/main/java/parrot/AfricanParrot.java
+++ Java/src/main/java/parrot/AfricanParrot.java
@@ -8,4 +8,13 @@ public class AfricanParrot extends ParrotImpl{
     static ParrotImpl createAfricanParrot(int numberOfCoconuts) {
         return new ParrotImpl(ParrotTypeEnum.AFRICAN, numberOfCoconuts, 0, false);
     }
+
+    @Override
+    public double getSpeed() {
+        return switch (type) {
+            case AFRICAN -> Math.max(0, Parrot.BASE_SPEED - getLoadFactor() * numberOfCoconuts);
+            case NORWEGIAN_BLUE -> (isNailed) ? 0 : getBaseSpeed(voltage);
+            default -> throw new IllegalArgumentException();
+        };
+    }
 }
