96182921c06749249c68a835a2f34b013f9622cc
Gregor Riegler <gregor.riegler@ebcont.com>
2023-02-10 15:01:40 +0100

    wip

--- Java/src/main/java/parrot/NorwegianBlueParrot.java
+++ Java/src/main/java/parrot/NorwegianBlueParrot.java
@@ -13,7 +13,7 @@ public class NorwegianBlueParrot extends Parrot {
 
     @Override
     public double getSpeed() {
-        return (isNailed) ? 0 : getBaseSpeed(voltage);
+        return isNailed ? 0 : getBaseSpeed(voltage);
     }
 
     private double getBaseSpeed(double voltage) {
