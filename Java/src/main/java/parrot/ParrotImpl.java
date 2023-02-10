854df12bbee5fec1a1c37b2aa519873767ad6fbc
Gregor Riegler <gregor.riegler@ebcont.com>
2023-02-10 14:04:23 +0100

    wip

--- Java/src/main/java/parrot/ParrotImpl.java
+++ Java/src/main/java/parrot/ParrotImpl.java
@@ -17,9 +17,9 @@ public class ParrotImpl implements Parrot {
     @Override
     public double getSpeed() {
         return switch (type) {
-            case EUROPEAN -> getBaseSpeed();
             case AFRICAN -> Math.max(0, getBaseSpeed() - getLoadFactor() * numberOfCoconuts);
             case NORWEGIAN_BLUE -> (isNailed) ? 0 : getBaseSpeed(voltage);
+            default -> throw new IllegalArgumentException();
         };
     }
 
