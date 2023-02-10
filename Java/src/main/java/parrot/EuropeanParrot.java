18d31aad771f44cfb9712d4688b70b8962e7efba
Gregor Riegler <gregor.riegler@ebcont.com>
2023-02-10 13:59:04 +0100

    wip

--- Java/src/main/java/parrot/EuropeanParrot.java
+++ Java/src/main/java/parrot/EuropeanParrot.java
@@ -2,6 +2,6 @@ package parrot;
 
 public class EuropeanParrot {
     static Parrot createEuropean() {
-        return new Parrot(ParrotTypeEnum.EUROPEAN, 0, 0, false);
+        return new ParrotImpl(ParrotTypeEnum.EUROPEAN, 0, 0, false);
     }
 }
